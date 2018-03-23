0e77392e3c1d11f94c0a3a4c97eff43dd2446e06
DogeSec <utdoge@gmail.com>
2018-03-23 00:00:00 -0600

    Update check.py

--- check.py
+++ check.py
@@ -1,6 +1,6 @@
 import datetime
+user_input = int(input("Enter the year you were born: "))
 def check():
-  user_input = int(input("Enter the year you were born: "))
   year = datetime.date.today().year
   print(f"You were born in {user_input). That was {user_input-year} years ago!)
 check()
